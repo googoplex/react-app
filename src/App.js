c33e0d08bedffe3c0cde079cd0b24750ef1fe39c
googoplex <kontolmeme512@gmail.com>
2020-12-29 22:46:39 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import Upload from './upload';
 import Home from './route/Home';
 import Hearing from './route/Hearing';
 import History from './route/History';
-import Result from './route/History';
+import Result from './route/Result';
 import Nav from './component/navigation';
 import {BrowserRouter as Router,Route,Switch,Link } from 'react-router-dom';
-
 
 function App() {
   return (
@@ -17,18 +16,16 @@ function App() {
     <div className="App">
      <h1>Pendut.Ph</h1>
      <Router>
-       <Switch>
-     <Nav/>
-     <Route  path="/home" component={Home}/>
-     <Route  path="/history" component={Result}/>
-     <Route  path="/history" component={History}/>
-     <Route  path="/hearing" component={Hearing}/>
-      </Switch>
+        <Nav/>
+        <Switch>
+
+          <Route  path="/home" component={Home}/>
+          <Route  path="/result" component={Result}/>
+          <Route  path="/history" component={History}/>
+          <Route  path="/hearing" component={Hearing}/>
+        </Switch>
       </Router>
-     {/* <Home/>
-     <Hearing/>
-     <History/> */}
-     {/* <Upload/> */}
+    
     
     </div>
     </React.Fragment>
--- src/component/navigation.js
+++ src/component/navigation.js
@@ -6,12 +6,12 @@ import { Link } from 'react-router-dom';
 function Nav(){
     return(
       
-        <div className ="bg-blue-50 h-20  flex items-center position-sticky font-serif color-gray">
+        <div className ="bg-blue-50 h-20  flex items-center position-sticky font-sans text-md subpixel-antialiased">
             <div className=" w-full flex  flex flex-row flex justify-center">
-            <ul className="flex flex-row  flex items-center   flex-shrink space-x-4">
-            <li><svg className="w-10 bg-blue-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 23" stroke="gray">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-</svg><Link to={"/home"}>HOME</Link></li>
+            <ul className="flex flex-row  flex items-center   flex-shrink space-x-6">
+            <li><svg className="w-8 h-8 bg-black-100" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 23 23" stroke="gray">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+            </svg><Link to={"/home"}>HOME</Link></li>
             <li><Link to={"/result"}>Swertres Result</Link></li>
             <li><Link to={"/history"}>Swertres History</Link></li>
             <li><Link to={"/hearing"}>Swertres Hearing</Link></li>
